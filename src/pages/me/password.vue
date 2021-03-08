3847dd964b378d68df1f178250d8996f6470fd97
touchitvoid <touchitvoid@gmail.com>
2021-03-09 01:47:21 +0800

    feat: 修改密码

--- src/pages/me/password.vue
+++ src/pages/me/password.vue
@@ -2,20 +2,20 @@
   <view>
     <status-bar title="密码"></status-bar>
     <custom-line custom type="custom">
-      <input placeholder="请输入旧密码" class="form-input" placeholder-class="placeholder_f" type="text">
+      <input v-model="form.old_password" placeholder="请输入旧密码" class="form-input" placeholder-class="placeholder_f" type="password">
     </custom-line>
     <view class="divider-16"></view>
     <custom-line custom type="custom">
-      <input placeholder="请输入新密码" class="form-input" placeholder-class="placeholder_f" type="text">
+      <input v-model="form.password" placeholder="请输入新密码" class="form-input" placeholder-class="placeholder_f" type="password">
     </custom-line>
     <view class="divider-16"></view>
     <custom-line custom type="custom">
-      <input placeholder="再次输入新密码" class="form-input" placeholder-class="placeholder_f" type="text">
+      <input v-model="form.confirm_password" placeholder="再次输入新密码" class="form-input" placeholder-class="placeholder_f" type="password">
     </custom-line>
     <view class="tips">
       注意：密码智能是字母+数字+特殊字符(!@#$%&*_.?)的组合，介于8-20位之间
     </view>
-    <custom-line type="button" title="保存"></custom-line>
+    <custom-line @click.native="patchPassword" type="button" title="保存"></custom-line>
     <view class="forget">忘记密码？</view>
   </view>
 </template>
@@ -23,6 +23,7 @@
 <script>
 import StatusBar from '@/components/custom-status-bar'
 import CustomLine from '@/components/custom-line.vue'
+import { PatchPassword } from '@/api'
 
 export default {
   name: "password",
@@ -32,7 +33,31 @@ export default {
   },
   data () {
     return {
-
+      form: {
+        old_password: '',
+        password: '',
+        confirm_password: ''
+      }
+    }
+  },
+  methods: {
+    async patchPassword() {
+      if (!this.form.old_password) return this.$toast('请输入旧密码')
+      if (!this.form.password) return this.$toast('请输入新密码')
+      if (this.form.password !== this.form.confirm_password) return this.$toast('请确定两次密码输入一致')
+      try {
+        const { data } = await PatchPassword(this.form)
+        if (data) {
+          this.form = {
+            old_password: '',
+            password: '',
+            confirm_password: ''
+          }
+          this.$toast('修改成功')
+        }
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
